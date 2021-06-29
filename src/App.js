ee8c02308f01c069cd787f65a411a3273e09e43d
heycryptobob <heycryptobob@gmail.com>
2021-06-29 07:58:08 +0200

    coming soon...

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import './App.scss';
-import Features from './Features';
-import Hero from './Hero';
-import Tokenomics from './Tokenomics';
-import Distribution from './Distribution';
-import Roadmap from './Roadmap';
+// import Features from './Features';
+// import Hero from './Hero';
+// import Tokenomics from './Tokenomics';
+// import Distribution from './Distribution';
+// import Roadmap from './Roadmap';
 // import Fund from './Fund';
 // import Contract from './Contract';
 // import Buy from './Buy';
 // import Team from './Team';
-import Footer from './Footer';
-import Header from './Header';
+// import Footer from './Footer';
+// import Header from './Header';
+import Soon from './Soon';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Soon />
+      {/* <Header />
       <Hero />
       <Features />
       <Tokenomics />
       <Distribution />
-      <Roadmap />
-      {/* <Fund /> */}
-      {/* <Team /> */}
-      {/* <Contract /> */}
-      {/* <Buy /> */}
-      <Footer />
+      <Roadmap /> */}
+      {/* <Fund />
+      <Team />
+      <Contract />
+      <Buy /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/Soon.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.png";
+import { Container, Row, Col } from "react-bootstrap";
+import Tada from "react-reveal/Tada";
+import { Telegram, TwitterSquare } from "@styled-icons/fa-brands";
+
+export default function Soon() {
+  const [touch, setTouch] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTouch(touch + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [touch]);
+
+  return (
+    <Container className="m-5 p-5 text-center">
+      <Row>
+        <Col>
+          <Tada spy={touch}>
+            <img src={logo} width="256" height="256" alt="logo" />
+          </Tada>
+        </Col>
+      </Row>
+      <Row className="pt-5">
+        <Col>
+          <h1 className="display-6">Launching Website at 600 Telegram Members</h1>
+        </Col>
+      </Row>
+      <Row className="pt-5">
+        <Col>
+          <a
+            href="https://t.me/RhinoCoinOfficial"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white"
+          >
+            <Telegram size={40} />
+          </a>
+          <a
+            href="https://twitter.com/RhinoCoinBSC"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white ms-3"
+          >
+            <TwitterSquare size={40} />
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
