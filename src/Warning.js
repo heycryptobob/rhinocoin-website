86aa317eeef0897f77ec6f10a67b777bcee26452
heycryptobob <heycryptobob@gmail.com>
2021-07-03 16:24:14 +0300

    dxsale link

--- src/Warning.js
+++ src/Warning.js
@@ -1,12 +1,18 @@
 import { Alert } from 'react-bootstrap'
-import Countdown from 'react-countdown';
+// import Countdown from 'react-countdown';
 
-const date = Date.parse('03 Jul 2021 13:00:00 GMT')
+// const date = Date.parse('03 Jul 2021 13:00:00 GMT')
 
 export default function Warning() {
   return (
     <Alert variant="success" className="mb-0 rounded-0 text-center">
-      Presale in <strong><Countdown date={date} /></strong>. Stay safe, only join the {" "}
+      Presale on {" "}
+      <Alert.Link
+      href="https://dxsale.app/app/v2_9/defipresale?saleID=244&chain=BSC"
+      target="_blank"
+        rel="noreferrer"
+      >DXSale</Alert.Link>
+      . Stay safe, only join the {" "}
       <Alert.Link
         href="https://twitter.com/RhinoCoinBSC"
         target="_blank"
